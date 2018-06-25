6686afbe1433cf05d9a14bcfa0b0ce5c62f35577
Aleksander Mozetic <aleksander.mozetic@gmail.com>
2018-06-25 18:08:20 +0200

    HttpClient, HttpHeaders

--- WebApplication2/ClientApp/app/components/fetchdata/fetchdata.component.ts
+++ WebApplication2/ClientApp/app/components/fetchdata/fetchdata.component.ts
@@ -1,28 +1,75 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Injectable, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
+class ApiData {
+    constructor(public id: number, public name: string, public dataAll: string, public dataSpectrum: string, public isComplete: boolean) { }
+}
+
+@Injectable()
+export class AudioDataService {
+
+    constructor(private http: HttpClient) { }
+
+    captureData(index: string): Observable<ApiData> {
+        return this.http.get<ApiData>('/api/Todo/' + index);
+    }
+}
+
 @Component({
     selector: 'fetchdata',
-    templateUrl: './fetchdata.component.html',
+    template: `
+        <h1>Fetch audio data</h1>
+        <blockquote><strong>ASP.NET Core 2.0 and Angular 5</strong>
+            <br>{{" Click update to fetch new data. "}}<button (click)="updateAudio()">Update</button>
+        </blockquote>
+        <p *ngIf="!data"><em>Loading...</em></p>
+        <table class='table' *ngIf="data">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>DataAll</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{ data.id }}</td>
+                    <td>{{ data.name }}</td>
+                    <td>{{ data.dataAll }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <table class='table' *ngIf="data">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>DataSpectrum</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{ data.id }}</td>
+                    <td>{{ data.name }}</td>
+                    <td>{{ data.dataSpectrum }}</td>
+                </tr>
+            </tbody>
+        </table>
+        `,
     styleUrls: ['./fetchdata.component.css']
 })
 export class FetchDataComponent {
     public data: ApiData | undefined;
     //public data: UserResponse | undefined;
-    //public forecasts: WeatherForecast[] | undefined;
 
     constructor(httpClient: HttpClient, @Inject('BASE_URL') baseUrl: string) {
         httpClient.get<ApiData>(baseUrl + 'api/Todo/21').subscribe(result => {
-                this.data = result;
-            }, error => console.error(error));
+            this.data = result;
+        }, error => console.error(error));
 
         /*httpClient.get<UserResponse>('https://api.github.com/users/seeschweiler').subscribe(result => {
             this.data = result;
-        }, error => console.error(error));*/
-
-        /*httpClient.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-            this.forecasts = result;
         }, error => console.error(error));*/
     }
 }
@@ -39,11 +86,4 @@ interface UserResponse {
     login: string;
     bio: string;
     company: string;
-}
-
-interface WeatherForecast {
-    dateFormatted: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
 }
